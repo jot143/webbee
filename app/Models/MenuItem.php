2b553c75cfa291453eda28381e0f52359299b919
Jot Coolmaster <jot143@live.in>
2021-05-31 19:36:42 +0530

    menu done

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -5,8 +5,11 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class MenuItem extends Model
 {
-
+    function children() {
+        return $this->hasMany(MenuItem::class, 'parent_id', 'id')->with('children');
+    }
 }
